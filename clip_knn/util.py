51dc9b049f538cf9537588051b688ff57ac85d57
Dominik Farhan <nikousf@seznam.cz>
2023-03-31 07:26:55 +0200

    allow incremental knn build

--- clip_knn/util.py
+++ clip_knn/util.py
@@ -17,7 +17,7 @@ class Prediction():
 class Model:
     def __init__(self, N, metric, train):
         self.train_path = train
-        X, y = self.get_train_data(train)
+        X, y = [], []
         self.knn = KNeighborsClassifier(N, metric=metric)
         self.knn.fit(X, y)
 
@@ -29,15 +29,26 @@ class Model:
             images_embeds = {filename: model.encode_image(images[filename]).numpy()[0] for filename in filenames}
         return images_embeds
 
-    def get_train_data(self, train_path):
+    def add_label_data(self, train_path, dr, knn_fit=False):
         y = []
         X = []
+        img_paths = [img_path for img_path in os.listdir(train_path + '/' + dr) if '.txt' not in img_path]
+        embs = get_embeddings(img_paths)
+        for emb in embs:
+            X.append(emb)
+        y += [dr] * len(embs)
+        self.X += X
+        self.y += y
+        if knn_fit:
+            self.knn_fit()
+
+    def get_train_data(self, train_path):
         for dr in os.listdir(train_path):
-            img_paths = [img_path for img_path in os.listdir(train_path + '/' + dr) if '.txt' not in img_path]
-            embs = get_embeddings(img_paths)
-            X += *embs
-            y += [dr] * len(embs)
-        return X, y
+            add_label_data(train_path, dr)
+        self.knn_fit()
+
+    def fit(self):
+        self.knn.fit(self.X, self.y)
 
     def merge(self, ls):
         out = []
